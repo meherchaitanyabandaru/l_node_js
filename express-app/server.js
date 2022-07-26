af39494566ee71f87697855e40845f3b357c46f3
MEHER CHAITANYA <47631184+meherchaitanyabandaru@users.noreply.github.com>
2022-07-26 12:59:19 +0530

    cleaned
    cleaned

---
+++ express-app/server.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const md5 = require('md5');
+const random = require('random-name');
+const { v4: uuidv4 } = require('uuid');
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/mdfive', (req, res) => {
+  let name = random.first();
+  res.send(name + " is encrypted as : " + md5(name)+ " with MD5 Algorithm")
+})
+
+app.get('/uuidtest', (req, res) => {
+  res.send(uuidv4())
+})
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
