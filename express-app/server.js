1c29e97260e3ffa32e81d82924bebedfd50f48a4
MEHER CHAITANYA <47631184+meherchaitanyabandaru@users.noreply.github.com>
2022-07-27 19:14:40 +0530

    express mongo working
    express mongo working

--- express-app/server.js
+++ express-app/server.js
@@ -3,7 +3,9 @@ const md5 = require('md5');
 const random = require('random-name');
 const { v4: uuidv4 } = require('uuid');
 const {encryptPlainText} =require('../express-app/utils/secure')
-
+require('./db/mongoose')
+const userRouter = require('./routes/user')
+const taskRouter = require('./routes/task')
 
 const app = express()
 const port = 3000
@@ -12,20 +14,9 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.get('/mdfive', (req, res) => {
-  let name = random.first();
-  res.send(name + " is encrypted as : " + md5(name)+ " with MD5 Algorithm")
-})
-
-app.get('/uuidtest', (req, res) => {
-  res.send(uuidv4())
-})
-
-
-app.get('/importtest', (req, res) => {
-  res.send(encryptPlainText("tst"))
-})
-
+app.use(express.json())
+app.use(userRouter)
+app.use(taskRouter)
 
 
 app.listen(port, () => {
