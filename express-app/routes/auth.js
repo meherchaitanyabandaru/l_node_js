3847352366be816f4af62929fa44c6b87f290b0a
MEHER CHAITANYA <47631184+meherchaitanyabandaru@users.noreply.github.com>
2022-07-29 01:28:07 +0530

    jwt sample included
    jwt sample included

--- express-app/routes/auth.js
+++ express-app/routes/auth.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const User = require('../models/user');
 const router = new express.Router();
-
+const jwt = require('jsonwebtoken');
 
 // Routes
 
@@ -23,11 +23,11 @@ const router = new express.Router();
  *                     email:
  *                       type: string
  *                       description: user email.
- *                       example: sowmya.chappidi02@gmail.com
+ *                       example: sowmya.chappidi02@gmail.coms
  *                     password:
  *                       type: string
  *                       description: user password.
- *                       example: sowmya1234
+ *                       example: sowmya123
 */
 
 router.post('/login', async (req, res) => {
@@ -36,7 +36,12 @@ router.post('/login', async (req, res) => {
     if (!user[0]?.email) {
       res.status(404).send({'Error-Message': 'invalid credentials'});
     } else {
-      res.status(200).send(user);
+      const token = jwt.sign({email: user[0]?.email, password: user[0]?.password}, 'shhhhh');
+      const authenticationResponse={
+        'authenticationStatus': true,
+        'BearerToken': token,
+      };
+      res.status(200).send(authenticationResponse);
     }
   } catch (e) {
     res.status(400).send(e);
