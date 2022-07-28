93009d7ae56a862b918d81bcc8675ea4bed9b105
MEHER CHAITANYA <47631184+meherchaitanyabandaru@users.noreply.github.com>
2022-07-28 17:42:12 +0530

    swagger included
    swagger included

--- express-app/routes/post.js
+++ express-app/routes/post.js
@@ -1,8 +1,39 @@
-const { request } = require('express');
+/* eslint-disable max-len */
+const {request} = require('express');
 const express = require('express');
 const timestamp = require('time-stamp');
 const Post = require('../models/post');
 const router = new express.Router();
+
+
+// Routes
+
+/**
+ * @swagger
+ * /posts:
+ *   post:
+ *     summary: Create a new Post.
+ *     responses:
+ *       201:
+ *         description: Created
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                     postname:
+ *                       type: string
+ *                       description: post name.
+ *                       example: my post title
+ *                     postdescription:
+ *                       type: string
+ *                       description: The post description.
+ *                       example: my sample post description
+ *                     postedby:
+ *                       type: string
+ *                       description: The posted by.
+ *                       example: MEHAR BANDARU
+*/
 
 router.post('/posts', async (req, res) => {
   req.body.postedon=timestamp('YYYY/MM/DD:mm:ss').toString();
@@ -16,6 +47,17 @@ router.post('/posts', async (req, res) => {
   }
 });
 
+
+// Routes
+/**
+ *  @swagger
+ *  /posts:
+ *  get:
+ *     description: "it will fetch all the posts information"
+ *     responses:
+ *        '200':
+ *          description: "success"
+*/
 router.get('/posts', async (req, res) => {
   try {
     const posts = await Post.find({});
@@ -25,6 +67,23 @@ router.get('/posts', async (req, res) => {
   }
 });
 
+/**
+ * @swagger
+ * /posts/{id}:
+ *   get:
+ *     summary: Retrieve a single JSONPlaceholder user.
+ *     description: Retrieve a single JSONPlaceholder user. Can be used to populate a user profile when prototyping or testing an API.
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: Numeric ID of the user to retrieve.
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Pet updated.
+ */
 router.get('/posts/:id', async (req, res) => {
   const _id = req.params.id;
 
@@ -79,3 +138,4 @@ router.delete('/posts/:id', async (req, res) => {
 });
 
 module.exports = router;
+
--- express-app/server.js
+++ express-app/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const swaggerJsdoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
 require('./db/mongoose');
 const userRouter = require('./routes/user');
 const taskRouter = require('./routes/task');
@@ -11,6 +13,26 @@ const port = 3000;
 
 // Get the default connection
 const db = mongoose.connection;
+
+// Swagger
+const swaggerOptions = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'My Express API',
+      version: '1.0.0',
+      name: 'MEHAR BANDARU',
+    },
+  },
+  // eslint-disable-next-line max-len
+  apis: ['./routes/*.js'], // files containing annotations as above
+};
+
+
+const swaggerDocs= swaggerJsdoc(swaggerOptions);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
+
 
 // Bind connection to error event (to get notification of connection errors)
 db.on('error', console.error.bind(console, 'connection error:'));
